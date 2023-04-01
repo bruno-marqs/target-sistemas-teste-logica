0686c47b3462888dc8f8f902d049aa1150767a0b
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-01 17:05:32 -0300

    calculando % + print result

--- questao-04.py
+++ questao-04.py
@@ -7,3 +7,18 @@ fat_outros = 19849.53
 
 # Faturamento total
 fat_total = fat_sp + fat_rj + fat_mg + fat_es + fat_outros
+
+# Percentual de cada Estado
+percent_sp = (fat_sp / fat_total) * 100
+percent_rj = (fat_rj / fat_total) * 100
+percent_mg = (fat_mg / fat_total) * 100
+percent_es = (fat_es / fat_total) * 100
+percent_outros = (fat_outros / fat_total) * 100
+
+# Imprimindo os resultados
+print("Percentual de representação no faturamento total:\n")
+print("Sao Paulo: ....... {:.2f}%".format(percent_sp))
+print("Rio de Janeiro: .. {:.2f}%".format(percent_rj))
+print("Minas Gerais: .... {:.2f}%".format(percent_mg))
+print("Espirito Santos: . {:.2f}%".format(percent_es))
+print("Outros Estados: .. {:.2f}%".format(percent_outros))
