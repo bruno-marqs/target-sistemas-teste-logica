32399fa5ce9c4e604bb01d06aaf366506c9b872b
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-01 17:33:29 -0300

    juntando char + print variavel

--- questao-05.py
+++ questao-05.py
@@ -6,4 +6,8 @@ lista_inversa = []
 for i in range(len(original) - 1, -1, -1):
     lista_inversa.append(original[i])
 
+# Juntando os caracteres numa nova String
+inversao = "".join(lista_inversa)
 
+# Imprimindo o resultado
+print(f"Frase/palavra invertida: {inversao}")
