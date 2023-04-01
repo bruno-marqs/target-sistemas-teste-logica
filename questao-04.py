b7a758f43c0440b72a401f28bd16373d2d84bfdf
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-01 16:57:37 -0300

    estabelecendo faturamento estadual + total

---
+++ questao-04.py
@@ -0,0 +1,9 @@
+# Faturamento por Estado
+fat_sp = 67836.43
+fat_rj = 36678.66
+fat_mg = 29229.88
+fat_es = 27165.48
+fat_outros = 19849.53
+
+# Faturamento total
+fat_total = fat_sp + fat_rj + fat_mg + fat_es + fat_outros
